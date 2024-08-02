9e3b2d62ab1e6eb03e5340c2b72d151e98b998e9
GhulamAkber <ghulamakber@codecross.co>
2024-08-02 15:21:37 -0700

    Minor Changes Done

--- qmaid-admin/flutter_service_worker.js
+++ qmaid-admin/flutter_service_worker.js
@@ -36,14 +36,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "4f0d93dd6e4e031f68c1b2218a1c4273
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "c03f3d0ebb16432226f28d697595a3e6",
+"flutter_bootstrap.js": "1317af91cce83b3b0aee50dc718bed12",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bb259604e3293f1fe76f9bcff73fac4b",
-"/": "bb259604e3293f1fe76f9bcff73fac4b",
-"main.dart.js": "f371b260c02d85d4c2abfbd37627ae71",
+"index.html": "05eaf7e67847e2a9b8e71b42d5975398",
+"/": "05eaf7e67847e2a9b8e71b42d5975398",
+"main.dart.js": "7476da78c174a2beae965b2b69da9da6",
 "manifest.json": "da9b6d818965aeb960164eaf1c49cbdc",
 "version.json": "bf0303fb0f0045b6a43caee39cabdd92"};
 // The application shell files that are downloaded before a service worker can
