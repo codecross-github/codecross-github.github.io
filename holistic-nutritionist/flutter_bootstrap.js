dc058898c5d7c2a5846ffad53b9b0497a4e272fc
cc-bilal <139101423+cc-bilal@users.noreply.github.com>
2024-11-29 22:07:19 +0500

    update Holistic Nutritionist

--- holistic-nutritionist/flutter_bootstrap.js
+++ holistic-nutritionist/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1052213497"
+    serviceWorkerVersion: "1153368992"
   }
 });
