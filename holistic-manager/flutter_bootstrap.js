fab9e6b108c815ed45318c1b4576284f65b6f1b0
cc-bilal <139101423+cc-bilal@users.noreply.github.com>
2024-12-27 22:00:52 +0500

    holistic issues and new features

--- holistic-manager/flutter_bootstrap.js
+++ holistic-manager/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3847925183"
+    serviceWorkerVersion: "3061066552"
   }
 });
