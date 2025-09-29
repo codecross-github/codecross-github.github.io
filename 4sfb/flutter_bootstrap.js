baa527167335964dc7ee28611cde0636c527685c
cc-bilal <139101423+cc-bilal@users.noreply.github.com>
2025-09-29 12:07:37 +0500

    deploy 4sfb dashboard

--- 4sfb/flutter_bootstrap.js
+++ 4sfb/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1227210067"
+    serviceWorkerVersion: "2670208486"
   }
 });
