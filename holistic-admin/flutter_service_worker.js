b9802730dde60b86766401fa4c6d7b05a5a54656
GhulamAkber368 <GhulamAkber@CodeCross.Co>
2026-08-27 20:42:16 +0500

    Added Parameters in URL

--- holistic-admin/flutter_service_worker.js
+++ holistic-admin/flutter_service_worker.js
@@ -70,14 +70,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bd5a1016729a4e8673fcaa09e7c45698
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "29c25bd90e3ae05613d15203c0bed8fd",
+"flutter_bootstrap.js": "2e75c40942a0ac46cfb35f2eaa829ccb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ebbfaeba91eb2c2f594a30ba8838ce8a",
 "/": "ebbfaeba91eb2c2f594a30ba8838ce8a",
-"main.dart.js": "bdd6307cc5aca6870c577ea40f37b979",
+"main.dart.js": "e302265fcc5fdfca2ab0f23f9ebe98bb",
 "manifest.json": "b53f0c545ccab0d0f66bef4abfe3972c",
 "version.json": "474de509ee9082d38deb8ac76cf548d3"};
 // The application shell files that are downloaded before a service worker can
