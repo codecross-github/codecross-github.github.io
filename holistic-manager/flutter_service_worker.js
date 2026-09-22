5923f208baa8e5e865c039c722954ef4f118f2d1
GhulamAkber368 <GhulamAkber@CodeCross.Co>
2026-09-22 12:22:18 +0500

    Done some optimizations for performance improvement in Manager Redesigned CRM

--- holistic-manager/flutter_service_worker.js
+++ holistic-manager/flutter_service_worker.js
@@ -70,14 +70,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bd5a1016729a4e8673fcaa09e7c45698
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "e18d8f239e80bbd39449e144a1b7d805",
+"flutter_bootstrap.js": "5e581edf2b5d79ab470ae0cb38bb888a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7a4095173926b1e815f987f2d14bff58",
 "/": "7a4095173926b1e815f987f2d14bff58",
-"main.dart.js": "018f88e668f712baec10e65aea50d46e",
+"main.dart.js": "73919525fb93051ed2b5213124784645",
 "manifest.json": "da2242ff8f8927f2cfcdda6d75f3d577",
 "version.json": "086ba2ed569037ff0f1e36d6578cd0dd"};
 // The application shell files that are downloaded before a service worker can
